e8573d4c97253b0e710b852fc8dcb18ce73935f6
hibo98 <niklasmerkelt@mail.de>
2020-02-13 07:38:43 +0100

    Don't search for allergies, when no customer was found

--- app/src/main/java/de/schkola/kitchenscanner/task/DatabaseCustomerTask.java
+++ app/src/main/java/de/schkola/kitchenscanner/task/DatabaseCustomerTask.java
@@ -6,6 +6,7 @@ import de.schkola.kitchenscanner.database.Allergy;
 import de.schkola.kitchenscanner.database.Customer;
 import de.schkola.kitchenscanner.database.LunchDatabase;
 import de.schkola.kitchenscanner.util.LunchResult;
+import java.util.ArrayList;
 import java.util.List;
 
 public class DatabaseCustomerTask extends AsyncTask<Integer, Void, LunchResult> {
@@ -21,7 +22,10 @@ public class DatabaseCustomerTask extends AsyncTask<Integer, Void, LunchResult> 
     @Override
     protected LunchResult doInBackground(Integer... integers) {
         Customer c = database.customerDao().getCustomer(integers[0]);
-        List<Allergy> a = database.allergyDao().getAllergies(c.xba);
+        List<Allergy> a = new ArrayList<>();
+        if (c != null) {
+            a = database.allergyDao().getAllergies(c.xba);
+        }
         return new LunchResult(c, a);
     }
 
