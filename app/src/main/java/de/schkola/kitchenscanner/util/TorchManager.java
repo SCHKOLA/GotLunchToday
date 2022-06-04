9e1fe04b32795324a2d1400149a5cc1ff70823d9
Niklas Merkelt <niklasmerkelt@mail.de>
2022-06-04 22:52:56 +0200

    Reformat TorchManager
    Signed-off-by: Niklas Merkelt <niklasmerkelt@mail.de>

--- app/src/main/java/de/schkola/kitchenscanner/util/TorchManager.java
+++ app/src/main/java/de/schkola/kitchenscanner/util/TorchManager.java
@@ -12,45 +12,48 @@ import java.util.concurrent.TimeUnit;
 
 public class TorchManager {
 
-    private ScheduledExecutorService s;
-    private CameraManager manager;
-    private AVCallback av;
-    private TCallback t;
+    private static final long TORCH_WAIT_TIME = 150;
+
+    private ScheduledExecutorService executorService;
+    private CameraManager cameraManager;
+    private AvailabilityCallback availabilityCallback;
+    private TorchCallback torchCallback;
 
     public TorchManager(Context context) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            s = Executors.newSingleThreadScheduledExecutor();
-            while (manager == null) {
-                manager = (CameraManager) context.getSystemService(Context.CAMERA_SERVICE);
+            executorService = Executors.newSingleThreadScheduledExecutor();
+            while (cameraManager == null) {
+                cameraManager = (CameraManager) context.getSystemService(Context.CAMERA_SERVICE);
             }
-            av = new AVCallback();
-            manager.registerAvailabilityCallback(av, null);
-            t = new TCallback();
-            manager.registerTorchCallback(t, null);
+            availabilityCallback = new AvailabilityCallback();
+            cameraManager.registerAvailabilityCallback(availabilityCallback, null);
+            torchCallback = new TorchCallback();
+            cameraManager.registerTorchCallback(torchCallback, null);
         }
     }
 
     public void shutdown() {
         //Reset torch mode
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            s.shutdownNow();
-            manager.unregisterAvailabilityCallback(av);
-            manager.unregisterTorchCallback(t);
+            executorService.shutdownNow();
+            cameraManager.unregisterAvailabilityCallback(availabilityCallback);
+            cameraManager.unregisterTorchCallback(torchCallback);
             try {
-                manager.setTorchMode("0", false);
+                cameraManager.setTorchMode("0", false);
             } catch (CameraAccessException ignored) {
+                // Empty on purpose
             }
         }
     }
 
     @RequiresApi(Build.VERSION_CODES.M)
-    private class AVCallback extends CameraManager.AvailabilityCallback {
+    private class AvailabilityCallback extends CameraManager.AvailabilityCallback {
 
         @Override
         public void onCameraAvailable(@NonNull String cameraId) {
             if (cameraId.equals("0")) {
                 try {
-                    manager.setTorchMode("0", true);
+                    cameraManager.setTorchMode("0", true);
                 } catch (CameraAccessException e) {
                     e.printStackTrace();
                 }
@@ -59,21 +62,21 @@ public class TorchManager {
     }
 
     @RequiresApi(Build.VERSION_CODES.M)
-    private class TCallback extends CameraManager.TorchCallback {
+    private class TorchCallback extends CameraManager.TorchCallback {
 
         @Override
         public void onTorchModeChanged(@NonNull String cameraId, boolean enabled) {
-            if (s.isShutdown()) {
+            if (executorService.isShutdown()) {
                 return;
             }
             if (cameraId.equals("0") && enabled) {
-                s.schedule(() -> {
+                executorService.schedule(() -> {
                     try {
-                        manager.setTorchMode("0", false);
+                        cameraManager.setTorchMode("0", false);
                     } catch (CameraAccessException e) {
                         e.printStackTrace();
                     }
-                }, 150, TimeUnit.MILLISECONDS);
+                }, TORCH_WAIT_TIME, TimeUnit.MILLISECONDS);
             }
         }
     }
